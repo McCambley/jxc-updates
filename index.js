b42975968a29b5dcad1e1af96945daecbca2aa15
McCambley <jakemccamb16@gmail.com>
2024-12-03 09:15:40 -0500

    Read trail report as text

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-console.log("Hello, World!");
- 
+import { load } from "cheerio";
+const URL = "https://www.jacksonxc.org/trail-report";
+async function readTrailReport() {
+  const response = await fetch(URL);
+  const trailReport = await response.text();
+  const $ = load(trailReport);
+
+  const reportBody = $("section.av_textblock_section");
+  const reportText = reportBody.text();
+
+  console.log(reportBody.text());
+
+  // TODO
+  // Summarize report text with OpenAI API
+  // Read grooming report PDF
+  // Summarize PDF with OpenAI API
+  // Send text update via Twilio
+  // Refactor this to work as GCF or Lambda Function
+}
+
+readTrailReport();
