f5906e49c45aff1c5a02d125825db917a413bfa6
McCambley <jakemccamb16@gmail.com>
2025-01-09 00:25:27 -0500

    Add CORS support and update ski report summary format

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ import OpenAI from "openai";
 import express from "express";
 import "dotenv/config";
 import sqlite3 from "sqlite3";
+import cors from "cors";
 
 const apiKey = process.env.OPENAI_API_KEY;
 const PORT = process.env.PORT || 3000;
 const systemPrompt =
-  "Translate the input into a concise, user-friendly text message for skiers. The message should include essential details such as trail conditions, grooming updates, special notes, and any relevant instructions or policies. Ensure the tone is clear, friendly, and professional. The message must not exceed 500 characters. Prioritize clarity and brevity. Sign off as SkiBot.";
+  "You summarize a nordic ski report for experienced nordic skiers. Summarize the input according to the following directions. DO NOT USER MARKDOWN. The report should include essential sections (not numbered): A bright salutation then 1) Today's Best Bet 2) Grooming Updates 3) Trail Conditions 4) Announcements (if applicable). The 'Best Bet' MUST tell skiers what trail to ski in order to have the best day. Unless it has recently snowed according to the report, or the report explicitly mentions soft snow on the best trail, suggest using skate skis. For the entire summary, ensure the tone is clear, friendly, and professional. The summary must not exceed 750 characters. Prioritize clarity and brevity. Sign off as SkiBot.";
 const URL = "https://www.jacksonxc.org/trail-report";
 
 const openai = new OpenAI({ apiKey });
@@ -16,6 +17,23 @@ const app = express();
 const db = new sqlite3.Database("ski_reports.db");
 const MINUTES = 1000 * 60;
 const MAX_AGE_MINUTES = 5;
+const allowedOrigins = [
+  "http://127.0.0.1:5500",
+  // "http://localhost:3000",
+  "https://mccambley.github.io/jxc-updates-frontend/",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+// app.use(cors(corsOptions));
 
 //  Initialize Database
 db.serialize(() => {
@@ -126,7 +144,7 @@ app.get("/", async (req, res) => {
   res.status(200).send({ status: "ok" });
 });
 
-app.get("/ski-report", async (req, res) => {
+app.get("/ski-report", cors(corsOptions), async (req, res) => {
   // Check for cached report
   const latestReport = await getLatestReport();
 
